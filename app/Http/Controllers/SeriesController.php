7bae0619f91c25deaaef01fc0d1ea64ebd8e9330
LeonardoCCastro <leonardo.correia@estudante.iftm.edu.br>
2023-06-28 22:10:47 -0300

    eps e temp da serie

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Serie;
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Season;
+use App\Models\Series;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = Serie::query()->orderBy('nome')->get();
+        $series = Series::all();
         $successMessage = $request->session()->get('success.message');
         $request->session()->forget('success.message');
-        return view('series.index')->with('series',$series)->with('successMessage',$successMessage);
+        return view('series.index')->with('series', $series)->with('successMessage', $successMessage);
     }
 
     public function create()
@@ -21,35 +23,59 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
         // $nomeSerie = $request->input('nome');
         // $serie = new Serie();
         // $serie->nome = $nomeSerie;
         // $serie->save();
-        $serie = Serie::create($request->all());
-        $request->session()->put('success.message',"Série {$serie->nome} criada com sucesso");
-        return to_route('series.index'); 
+        $serie = Series::create($request->all());
+        
+        $seasons = [];
+        for ($i = 1; $i <= $request->seasonsQty; $i++) {
+            $seasons[] = [
+                'series_id' => $serie->id,
+                'number' => $i,
+            ];
+        }
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach($serie->seasons as $season)
+        {
+            for($j = 1; $j <= $request->seasonsQty; $j++)
+            {
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $j,
+                ];
+            }
+        }
+        Episode::insert($episodes);
+        
+
+        $request->session()->put('success.message', "Série {$serie->nome} criada com sucesso");
+        return to_route('series.index');
     }
 
-    public function destroy(Serie $serie, Request $request)
+    public function destroy(Series $serie, Request $request)
     {
-        $serie->delete(); 
-        $request->session()->put('success.message',"Série {$serie->nome} removida com sucesso");
-        return to_route('series.index'); 
+        $serie->delete();
+        $request->session()->put('success.message', "Série {$serie->nome} removida com sucesso");
+        return to_route('series.index');
     }
 
-    public function edit(Serie $serie)
+    public function edit(Series $serie)
     {
-        return view('series.edit')->with('serie',$serie);
+        return view('series.edit')->with('serie', $serie);
     }
 
-    public function update(Serie $serie, Request $request)
+    public function update(Series $serie, SeriesFormRequest $request)
     {
         $serie->fill($request->all());
         $serie->save();
-        $request->session()->put('success.message',"Série {$serie->nome} atualizada com sucesso");
-        return to_route('series.index'); 
+        $request->session()->put('success.message', "Série {$serie->nome} atualizada com sucesso");
+        return to_route('series.index');
     }
 
 }
--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,3 +1,10 @@
-<x-layout title="Editar Série {{$serie->nome}}">
-    <x-series.form action="{{route('series.update',$serie->id)}}" nome="{{$serie->nome}}"/>
+<x-layout title="Editar Série {{ $serie->nome }}">
+    <form action="{{ route('series.update', $serie->id) }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome:</label>
+            <input type="text" id="nome" name="nome" class="form-control" value="{{ $serie->nome }}">
+        </div>
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+    </form>
 </x-layout>
